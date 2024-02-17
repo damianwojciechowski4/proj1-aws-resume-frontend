0b983d3822703e12d4525b1ddc88875704d353b0
damianwojciechowski4 <110458027+damianwojciechowski4@users.noreply.github.com>
2024-02-17 13:31:21 +0100

    Update script.js

--- front-end/scripts/script.js
+++ front-end/scripts/script.js
@@ -9,7 +9,7 @@
  */
 
 
-const API_ENDPOINT = 'https://hfir4vopgd.execute-api.eu-central-1.amazonaws.com/dev';
+const API_ENDPOINT = 'https://9q8s3rkd10.execute-api.eu-central-1.amazonaws.com/devStage_cloudResume/VisitorsCounter';
 
 
 const XMR = new XMLHttpRequest()
